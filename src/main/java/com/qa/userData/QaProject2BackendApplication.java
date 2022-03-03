e7e01d2e34c88487e2e6906243939d29fd3a0a31
AidanHassett <aidanhassett.personal@gmail.com>
2022-03-03 10:14:52 +0000

    Initial Commit

---
+++ src/main/java/com/qa/userData/QaProject2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.qa.userData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaProject2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaProject2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/userData/QaProject2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.userData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaProject2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
